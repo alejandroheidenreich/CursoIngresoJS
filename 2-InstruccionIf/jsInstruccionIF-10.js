e3c495f948bdff867917aad5f68dcc8e1e98fcfd
alejandroheidenreich <heidenreich.alejandro@gmai.com>
2021-07-09 03:14:05 -0300

    randoms

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,19 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	let nota;
 
-}//FIN DE LA FUNCIÓN
+	nota = Math.floor((Math.random() * (10 - 1 + 1)) + 1);
+
+	if (nota > 8){
+		alert("EXCELENTE");
+	}
+	else{
+		if (nota <= 9 && nota >= 4){
+			alert("APROBÓ");
+		}
+		else{
+			alert("Vamos, la proxima se puede");
+		}
+	}
+	
+}
